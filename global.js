a1a98d652d67784d221838e0c02ab6cfb3a32e35
PendingMitch <70475488+PendingMitch@users.noreply.github.com>
2022-01-30 12:13:49 +0000

    Updated header for mobile responsiveness

--- global.js
+++ global.js
@@ -12,3 +12,19 @@ function upgradeLog(txt) {
 function dataLog(txt) {
     console.log('%c'+txt, "color:red;")
 }
+
+
+const header = document.getElementById('displayInfo')
+window.onscroll = function () {
+    let header_position = window.getComputedStyle(header).getPropertyValue('position')
+    if (header_position == "sticky") {
+        var top = window.scrollY;
+        if (top >= 60) {
+            header.classList.remove('not_scrolled')
+            header.classList.add('scrolled')
+        } else {
+            header.classList.remove('scrolled')
+            header.classList.add('not_scrolled')
+        }
+    }
+}
